b8c6c2483bba2eb80222a29a9d31112b940700c3
keitax <keitax913@yahoo.co.jp>
2017-04-02 22:16:23 +0900

    config: Add the site-title property

--- config/config.go
+++ config/config.go
@@ -1,5 +1,24 @@
 package config
 
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
 type Config struct {
-	DatabaseDir string
+	SiteTitle   string `yaml:"site-title"`
+	DatabaseDir string `yaml:"database-dir"`
+}
+
+func Parse(configFile string) (*Config, error) {
+	bs, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return nil, err
+	}
+	var c Config
+	if err := yaml.Unmarshal(bs, &c); err != nil {
+		return nil, err
+	}
+	return &c, nil
 }
---
+++ config/config_test.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "config-test-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+
+	if _, err := f.WriteString(`
+site-title: Textvid Blog
+database-dir: /usr/var/textvid/database
+`); err != nil {
+		t.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	parsed, err := Parse(f.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := &Config{
+		SiteTitle:   "Textvid Blog",
+		DatabaseDir: "/usr/var/textvid/database",
+	}
+	if !reflect.DeepEqual(expected, parsed) {
+		t.Errorf("Failed to parse: expected: %v, parsed: %v", expected, parsed)
+	}
+}
