77a5bb053d8de9454228d484748fe487d0ef8a36
keitax <keitax913@yahoo.co.jp>
2017-04-21 02:47:51 +0900

    dao: Add the fields keeping neighbor posts to the Post type

--- entity/post.go
+++ entity/post.go
@@ -3,11 +3,13 @@ package entity
 import "time"
 
 type Post struct {
-	Id        int64      `db:"id"`
-	CreatedAt *time.Time `db:"created_at"`
-	UpdatedAt *time.Time `db:"updated_at"`
-	UrlName   string     `db:"url_name"`
-	Title     string     `db:"title"`
-	Body      string     `db:"body"`
-	Labels    []string
+	Id           int64      `db:"id"`
+	CreatedAt    *time.Time `db:"created_at"`
+	UpdatedAt    *time.Time `db:"updated_at"`
+	UrlName      string     `db:"url_name"`
+	Title        string     `db:"title"`
+	Body         string     `db:"body"`
+	Labels       []string
+	NextPost     *Post
+	PreviousPost *Post
 }
