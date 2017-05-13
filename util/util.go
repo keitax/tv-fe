c29db26f1d7d45eddbb3705cf315a5e09d8c96c2
keitax <keitax913@yahoo.co.jp>
2017-05-13 21:45:47 +0900

    util: Implement the StripMetadata function

--- util/util.go
+++ util/util.go
@@ -3,10 +3,14 @@ package util
 import (
 	"html/template"
 	"os"
+	"strings"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday"
+	"gopkg.in/yaml.v2"
 )
+
+const metadataSeparator = "---"
 
 func ExistsFile(path string) bool {
 	_, err := os.Stat(path)
@@ -23,4 +27,56 @@ func ParseMarkdown(text string) template.HTML {
 	bs := blackfriday.MarkdownBasic([]byte(text))
 	bs = bluemonday.UGCPolicy().SanitizeBytes(bs)
 	return template.HTML(string(bs))
+}
+
+func StripMetadata(content string) (map[string]interface{}, string) {
+	lineno := 0
+	lines := strings.Split(content, "\n")
+
+	isEof := func() bool {
+		return lineno >= len(lines)
+	}
+	parseMetadataSeparator := func() bool {
+		if isEof() {
+			return false
+		}
+		if lines[lineno] == metadataSeparator {
+			lineno++
+			return true
+		}
+		return false
+	}
+	parseMetadataBody := func() map[string]interface{} {
+		ls := []string{}
+		for !isEof() {
+			if lines[lineno] == metadataSeparator {
+				break
+			}
+			ls = append(ls, lines[lineno])
+			lineno++
+		}
+		metadata := map[string]interface{}{}
+		if err := yaml.Unmarshal([]byte(strings.Join(ls, "\n")), &metadata); err != nil {
+			return metadata
+		}
+		return metadata
+	}
+	parseBody := func() string {
+		ls := []string{}
+		for !isEof() {
+			ls = append(ls, lines[lineno])
+			lineno++
+		}
+		return strings.Join(ls, "\n")
+	}
+
+	emptyMetadata := map[string]interface{}{}
+	if !parseMetadataSeparator() {
+		return emptyMetadata, parseBody()
+	}
+	metadata := parseMetadataBody()
+	if !parseMetadataSeparator() {
+		return emptyMetadata, parseBody()
+	}
+	return metadata, parseBody()
 }
--- util/util_test.go
+++ util/util_test.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"html/template"
+	"reflect"
 	"testing"
 )
 
@@ -17,5 +18,76 @@ hello, world!
 `)
 	if expected != result {
 		t.Errorf("Failed to parse:\nexpected:\n%v\nresult:\n%v", expected, result)
+	}
+}
+
+func TestStripMetadata(t *testing.T) {
+	testCases := []struct {
+		descr            string
+		input            string
+		expectedMetadata map[string]interface{}
+		expectedBody     string
+	}{
+		{
+			descr: "valid content",
+			input: `---
+title: Hello, World
+labels:
+  - hello
+  - world
+---
+hello, world
+`,
+			expectedMetadata: map[string]interface{}{
+				"title": "Hello, World",
+				"labels": []interface{}{
+					"hello",
+					"world",
+				},
+			},
+			expectedBody: "hello, world\n",
+		},
+		{
+			descr: "broken metadata (no first separator)",
+			input: `title: Hello, World
+---
+hello, world
+`,
+			expectedMetadata: map[string]interface{}{},
+			expectedBody: `title: Hello, World
+---
+hello, world
+`,
+		},
+		{
+			descr: "broken metadata (no second separator)",
+			input: `---
+title: Hello, World
+hello, world
+`,
+			expectedMetadata: map[string]interface{}{},
+			expectedBody:     "",
+		},
+		{
+			descr: "broken metadata (broken yaml)",
+			input: `---
+title: Hello
+ labels:
+---
+hello, world
+`,
+			expectedMetadata: map[string]interface{}{},
+			expectedBody:     "hello, world\n",
+		},
+	}
+
+	for _, tc := range testCases {
+		metadata, body := StripMetadata(tc.input)
+		if !reflect.DeepEqual(metadata, tc.expectedMetadata) {
+			t.Errorf("%s: StripMetadata() = %#v, _, expected %#v", tc.descr, metadata, tc.expectedMetadata)
+		}
+		if body != tc.expectedBody {
+			t.Errorf("%s: StripMetadata() = _, %#v, expected %#v", tc.descr, body, tc.expectedBody)
+		}
 	}
 }
