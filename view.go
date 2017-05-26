bb94af977d0f2c3f1d986c84ac5adc616a6ae275
keitax <keitax913@yahoo.co.jp>
2017-05-27 00:47:32 +0900

    Make to parse all templates

--- view.go
+++ view.go
@@ -3,7 +3,9 @@ package textvid
 import (
 	"html/template"
 	"io"
+	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -15,17 +17,6 @@ type View struct {
 }
 
 func (v *View) Render(w io.Writer) {
-	ts := template.New("root").Funcs(template.FuncMap{
-		"RenderMarkdown": ParseMarkdown,
-		"ShowTime": func(t time.Time) string {
-			return t.Format("Jan. 02, 2006, 3:04 PM")
-		},
-	})
-	ts = template.Must(ts.ParseFiles(
-		filepath.Join(v.config.TemplateDir, v.templateName),
-		filepath.Join(v.config.TemplateDir, "_header.tmpl"),
-		filepath.Join(v.config.TemplateDir, "_footer.tmpl"),
-	))
 	context_ := map[string]interface{}{
 		"SiteTitle":  v.config.SiteTitle,
 		"SiteFooter": v.config.SiteFooter,
@@ -34,7 +25,32 @@ func (v *View) Render(w io.Writer) {
 	for key, value := range v.context {
 		context_[key] = value
 	}
-	if err := ts.ExecuteTemplate(w, v.templateName, context_); err != nil {
+	if err := v.loadTemplates().ExecuteTemplate(w, v.templateName, context_); err != nil {
 		panic(err)
 	}
+}
+
+func (v *View) loadTemplates() *template.Template {
+	fs := []string{}
+	if err := filepath.Walk(v.config.TemplateDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if strings.HasSuffix(path, ".tmpl") {
+			fs = append(fs, path)
+		}
+		return nil
+	}); err != nil {
+		panic(err)
+	}
+	ts, err := template.New("root").Funcs(template.FuncMap{
+		"RenderMarkdown": ParseMarkdown,
+		"ShowTime": func(t time.Time) string {
+			return t.Format("Jan. 02, 2006, 3:04 PM")
+		},
+	}).ParseFiles(fs...)
+	if err != nil {
+		panic(err)
+	}
+	return ts
 }
