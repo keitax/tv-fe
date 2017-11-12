2331282c8483ead6777ebb168d76355587d1adcd
keitax <keitax913@yahoo.co.jp>
2017-11-12 18:41:52 +0900

    Remove admin contoller

--- application.go
+++ application.go
@@ -43,14 +43,12 @@ func NewApplication(config *Config) (http.Handler, error) {
 	c.Start()
 
 	pc := NewPostController(re, vs, ub, config)
-	ac := NewAdminController(re, vs, config)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", pc.GetIndex).Methods(http.MethodGet)
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(config.StaticDir)))).Methods(http.MethodGet)
 	r.HandleFunc("/posts/", pc.GetList).Methods(http.MethodGet)
 	r.HandleFunc("/{year:[0-9]{4}}/{month:0[1-9]|1[0-2]}/{name}.html", pc.GetSingle).Methods(http.MethodGet)
-	r.HandleFunc("/admin", ac.GetIndex).Methods(http.MethodGet)
 
 	app := negroni.New()
 	app.UseHandler(r)
