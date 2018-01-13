2211c2f30b2dde7ca2b6856e7c595def86cba308
keitax <keitax913@yahoo.co.jp>
2018-01-13 15:06:30 +0900

    Remove the toml config parser

--- config.go
+++ config.go
@@ -1,11 +1,7 @@
 package tvfe
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
-
-	"github.com/BurntSushi/toml"
 )
 
 // Run-level flags.
@@ -16,31 +12,15 @@ const (
 
 // Config structure of Textvid application.
 type Config struct {
-	SiteTitle           string `toml:"site-title"`
-	TemplateDir         string `toml:"template-dir"`
-	StaticDir           string `toml:"static-dir"`
-	SiteFooter          string `toml:"site-footer"`
-	Locale              string `toml:"locale"`
-	BaseURL             string `toml:"base-url"`
-	RunLevel            string `toml:"run-level"`
-	LocalGitRepository  string `toml:"local-git-repository"`
-	RemoteGitRepository string `toml:"remote-git-repository"`
-}
-
-// Parse reads a config file.
-func Parse(configFile string) (*Config, error) {
-	bs, err := ioutil.ReadFile(configFile)
-	if err != nil {
-		return nil, err
-	}
-	var c Config
-	if err := toml.Unmarshal(bs, &c); err != nil {
-		return nil, err
-	}
-	if !(c.RunLevel == ProductionRunLevel || c.RunLevel == DevelopmentRunLevel) {
-		return nil, fmt.Errorf("run-level must be %#v or %#v", ProductionRunLevel, DevelopmentRunLevel)
-	}
-	return &c, nil
+	SiteTitle           string
+	TemplateDir         string
+	StaticDir           string
+	SiteFooter          string
+	Locale              string
+	BaseURL             string
+	RunLevel            string
+	LocalGitRepository  string
+	RemoteGitRepository string
 }
 
 // GetFromEnv gets a config from environment vars.
--- config_test.go
+++ config_test.go
@@ -1,89 +1,9 @@
 package tvfe
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
 	"os"
-	"reflect"
 	"testing"
 )
-
-func TestParse(t *testing.T) {
-	f, err := ioutil.TempFile(os.TempDir(), "config-test-")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(f.Name())
-
-	if _, err := f.WriteString(`
-site-title = "Textvid Blog"
-template-dir = "./templates"
-static-dir = "./static"
-site-footer = "Copyright &copy; YOUR NAME"
-locale = "UTC"
-base-url = "http://localhost/"
-run-level = "production"
-local-git-repository = "/tmp/textvid-blog"
-remote-git-repository = "https://host.git/textvid-blog.git"
-`); err != nil {
-		t.Fatal(err)
-	}
-	if err := f.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	parsed, err := Parse(f.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := &Config{
-		SiteTitle:           "Textvid Blog",
-		TemplateDir:         "./templates",
-		StaticDir:           "./static",
-		SiteFooter:          "Copyright &copy; YOUR NAME",
-		Locale:              "UTC",
-		BaseURL:             "http://localhost/",
-		RunLevel:            ProductionRunLevel,
-		LocalGitRepository:  "/tmp/textvid-blog",
-		RemoteGitRepository: "https://host.git/textvid-blog.git",
-	}
-	if !reflect.DeepEqual(expected, parsed) {
-		t.Errorf("Failed to parse: expected: %v, parsed: %v", expected, parsed)
-	}
-}
-
-func TestParseToFailWhenInvalidRunLevel(t *testing.T) {
-	testCases := []struct {
-		descr       string
-		expectedErr error
-		runLevel    string
-	}{
-		{"valid runlevel", nil, "production"},
-		{"valid runlevel", nil, "development"},
-		{"invalid runlevel", errors.New(`run-level must be "production" or "development"`), "foobar"},
-	}
-
-	for _, tc := range testCases {
-		f, err := ioutil.TempFile(os.TempDir(), "config-test-")
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer os.Remove(f.Name())
-		if _, err := f.WriteString(fmt.Sprintf(`run-level = "%s"`, tc.runLevel)); err != nil {
-			t.Fatal(err)
-		}
-		if err := f.Close(); err != nil {
-			t.Fatal(err)
-		}
-
-		_, err = Parse(f.Name())
-		if !reflect.DeepEqual(err, tc.expectedErr) {
-			t.Errorf("%s: Parse() = %#v, expected %#v", tc.descr, err, tc.expectedErr)
-		}
-	}
-}
 
 func TestGetFromEnv(t *testing.T) {
 	os.Setenv("TV_SITE_TITLE", "textvid blog")
